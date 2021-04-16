d22fc5a31d518e6901d57a859b86aefcc3987c22
Ignacio <joseignacio439@gmail.com>
2021-04-16 16:42:01 -0300

    MasEjercicios
    Matrices

---
+++ TP1_Matrices/Matrices.h
@@ -0,0 +1,8 @@
+#ifndef MATRICES_H
+#define MATRICES_H
+
+int sumaDiagonalSuperior(const int matriz [][3], int orden);
+int sumaDiagonal(const int matriz[][3], int orden);
+int sumaDiagonalSecundaria(const int matriz[][3], int orden);
+
+#endif // MATRICES_H
