8eeb63ac6111d166d74ff0a21c3e464acfffeb7e
Ignacio <joseignacio439@gmail.com>
2021-04-16 16:42:01 -0300

    MasEjercicios
    Matrices

--- TP1_Matrices/main.c
+++ TP1_Matrices/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Matrices.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    int mat[3][3]={ {1,2,3},{1,2,3},{1,2,3} };
+    unsigned orden = 3;
+
+    printf("Suma diagonal secundaria: %d\n", sumaDiagonal(mat, orden));
+
+
     return 0;
 }
